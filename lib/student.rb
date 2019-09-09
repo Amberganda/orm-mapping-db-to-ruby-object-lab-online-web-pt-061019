5d24996a79530b5a894bcca6ece7f31ae0803d7b
Amber Taranto <ambzzzz@gmail.com>
2019-09-09 01:23:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,11 +27,9 @@ class Student
         LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, name).map do
-        |row|
-            self.new_from_db(row)
-        end.first
-    end
+    DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+    end.first
   end
 
   def save
